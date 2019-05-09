e97756bb27e08eb2075eeb26ce8f5fb7ae63e2fb
Ji Wanpeng <164981154@qq.com>
2019-05-09 11:50:48 +0800

    add hello4.c

---
+++ 7/hello4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<curses.h>
+
+int main()
+{	
+	int i;
+	initscr();
+	clear();
+	
+	for(i=0; i<LINES; i++){
+		move(i, i+i);
+		if(i%2 == 1)
+			standout();
+		addstr("Hello World");
+		if(i%2 == 1)
+			standend();
+		refresh();
+		sleep(1);
+		move(i, i+i);
+		addstr("                           ");
+	}	
+
+	endwin();
+	return 0;
+}
